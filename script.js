3486731040683775a58c81130a883619ef4d3431
lyu-cell <shizueizawa340@gmail.com>
2025-03-02 03:03:25 +0600

    feat: add a function for creating a bst graph representing the chess board specifically

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function knightLader(move, edge) {
 
 /*Only filters moves returned by 'knightLader' that has went outside the board
 */
+
 function possibleMoves(edge) {
     let move = []
     let validMoves = []
@@ -41,7 +42,6 @@ function possibleMoves(edge) {
 
 
 
-console.log("result: ", possibleMoves([3, 3]))
 
 
 
@@ -57,8 +57,127 @@ function includeArray(container, array) {
 }
 
 
-//function edgeListMaker() {
-//    let edgeList = []
-//
-//    if()
-//}
+
+// this function is used to filter our any duplicate edges that 
+// "possibleMoves" output might throw
+function filter(storage, item) {
+    let duplicate = false
+    for (let i = 0; i < storage.length; i++) {
+        if(storage[i].toString() === item.toString()) duplicate = true
+    }
+
+    if(duplicate === false) storage.push(item)
+}
+
+
+
+//this function is used to encase the possibleMoves output and the current edge which is used to get
+// those outputs from "possibleMoves" into an object.
+function d(edge) {
+    let result = possibleMoves(edge)
+    let storage = []
+    
+    storage.push(result[0])
+
+    if(result.length !== 0) {
+        for (let i = 0; i < result.length; i++) {
+            filter(storage, result[i])
+        }
+    }
+
+    return storage
+}
+
+
+
+
+
+
+
+
+
+let root = d([2, 1])
+
+
+
+function  show(index, child, mother) {
+    let childVer = d(child)
+    console.log(childVer)
+
+    mother.children[0][index] = childVer
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this function takes in knightPosition and destination 
+// then it step by step produces a BST and stops when the bst gets the destination node  
+function buildKnightTree(knightPosition, destination) {
+    let mother = {data: knightPosition}
+    let queue = [mother]
+
+    function lastNodeProcessor() {
+        let lastNode = queue.shift()
+ 
+        if(lastNode.data.toString() === destination.toString()) return
+        else {
+            let possibleChildren = d(lastNode.data)
+            let possibleChildrenObj = []
+            for (let i = 0; i < possibleChildren.length; i++) {
+                possibleChildrenObj.push({data: possibleChildren[i]})
+            }
+
+            lastNode.children = possibleChildrenObj
+            possibleChildrenObj.forEach((e) => queue.push(e))
+            return lastNodeProcessor()
+        }
+    }
+
+    lastNodeProcessor()
+
+    return mother
+}
+let mother = buildKnightTree([2, 1], [3, 3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
