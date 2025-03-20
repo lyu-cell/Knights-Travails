9b67991c3d463168e66e6453930818572654f1af
lyu-cell <shizueizawa340@gmail.com>
2025-03-20 15:00:47 +0600

    feat(function): add the relevent functions for searching the bst, processing and retriving the shortest path

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // this function returns an array of knight moves possible from the input which is given to it
 // it takes in the current location of the knight and returns a range of possible moves form that location
 // it doesn't include any duplicate moves or repeated moves or moves that goes outside the board 
-function knightMoves(edge) {
+function knightMovesPossible(edge) {
     let possibleMoves = []
     let validMoveStr = []   
     let validMoves = []
@@ -31,31 +31,102 @@ function knightMoves(edge) {
 
 
 
+
 // this function takes in knightPosition and destination 
 // then it step by step produces a BST and stops when the bst gets the destination node  
 function buildKnightTree(knightPosition, destination) {
     let mother = {data: knightPosition}
     let queue = [mother]
 
-    function lastNodeProcessor() {
+    function bstNodeBlockAdder() {
         let lastNode = queue.shift()
         if(lastNode.data.toString() === destination.toString()) return
         else {
-            let possibleMoves = knightMoves(lastNode.data)
+            let possibleMoves = knightMovesPossible(lastNode.data)
             lastNode.children = possibleMoves
             for (let i = 0; i < possibleMoves.length; i++) {
                 queue.push(possibleMoves[i])
             }
-
-            return lastNodeProcessor()
+    
+            return bstNodeBlockAdder()
         }
     }
-
-    lastNodeProcessor()
-
+    
+    bstNodeBlockAdder()
     return mother
 }
 
-let mother = buildKnightTree([2, 1], [3, 3])
 
-console.log(mother)
+
+
+
+
+
+
+
+function knightMoves(currentLocation, finalLocation) {
+    let mother = buildKnightTree(currentLocation, finalLocation)
+    let mainInventory = []
+    let activeInventory = []
+    let finalEdge = finalLocation
+
+    // this is a DFS algorithm which traverses the inputted branch
+    // the function only the path at the end of which exist the destination edge
+    // this function sends that whole path to the mainInventory from activeInventory
+    // the idea here is to store each path that has dest edge in it 
+    // and compare them to get the shortest path from the current knight location to the destination
+
+    function searchAndRecord(branch) {
+        let branchData = branch.data.toString()
+        let destEdge = finalEdge.toString()
+    
+        if (branchData === destEdge) {
+            mainInventory.push(freeLoop(activeInventory, branch.data))
+            return
+        } else if (branch.children === undefined) {
+            return
+        } else {
+            activeInventory.push(branch.data)
+            for (let i = 0; i < branch.children.length; i++) {
+                searchAndRecord(branch.children[i])            
+            }
+            activeInventory.pop()
+            return
+        }
+    }
+    
+    function freeLoop(mainArray, extraValue) {
+        let a = []
+    
+        for (let i = 0; i < mainArray.length; i++) {
+            a.push(mainArray[i])
+        }
+    
+        a.push(extraValue)
+        return a
+    }
+    
+    searchAndRecord(mother)
+
+    return shortestPath(mainInventory)[1]
+}
+
+
+// the function takes in an array of lists and returns the shortest array among them.
+// it is so that we can get the shortest path among all the paths which the searchAndRecord 
+// function returns
+function shortestPath(edgeLists) {
+    let shortestList = [edgeLists[0].length, edgeLists[0]]
+    edgeLists.shift()
+
+    for (let i = 0; i < edgeLists.length; i++) {
+        if(edgeLists[i].length < shortestList[0]) {
+            shortestList[0] = edgeLists[i].length;
+            shortestList[1] = edgeLists[i];
+        }
+    }
+    
+    return shortestList
+}
+
+console.log(knightMoves([5, 0], [5, 1]))
